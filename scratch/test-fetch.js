9f7c35145408acd860d8f60408367320eb71f1cf
Rehan Suman <rehansuman41008@gmail.com>
2026-07-12 16:55:38 +0530

    update project description and tech stack documentation for Odoo Hackathon submission

--- scratch/test-fetch.js
+++
@@ -1,8 +0,0 @@
-fetch("http://localhost:3000/api/auth/register", {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({ name: "test", email: "test2@example.com", password: "password123" })
-})
-.then(res => res.json())
-.then(data => console.log(data))
-.catch(err => console.error(err));
